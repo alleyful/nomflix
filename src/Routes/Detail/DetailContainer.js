b690a9bec6604ddfe3e0f2af7618cdc588a7b9f2
alleyful <alleyful@naver.com>
2019-08-30 00:28:52 +0900

    clean

---
+++ src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import DetailPresenter from './DetailPresenter';
+import { moviesApi, tvApi } from "../../api";
+
+export default class extends React.Component {
+	constructor(props) {
+		super(props);
+		const { location: { pathname } } = props;
+
+		this.state = {
+			result: null,
+			error: null,
+			loading: true,
+			isMovie: pathname.includes('/movie/')
+		}
+	}
+
+
+
+	async componentDidMount() {
+		const {
+			match: {
+				params: { id }
+			},
+			history: { push }
+		} = this.props;
+
+		const { isMovie } = this.state;
+		const parsedId = parseInt(id);
+		let result = null;
+
+		isNaN(parsedId) && push('/');
+
+		try {
+			({ data: result } = isMovie
+				? await moviesApi.movieDetail(parsedId)
+				: await tvApi.showDetail(parsedId));
+		} catch {
+			this.setState({
+				error: "Can't find anything."
+			})
+		} finally {
+			this.setState({
+				loading: false,
+				result
+			})
+		}
+	}
+
+	render() {
+		const { result, error, loading } = this.state;
+
+		return (
+			<DetailPresenter
+				result={result}
+				error={error}
+				loading={loading}
+			/>
+		)
+	}
+}
---
+++ src/Routes/Search/SearchPresenter.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Helmet from 'react-helmet';
+import Loader from 'Components/Loader';
+import Section from "Components/Section";
+import Message from 'Components/Message';
+import Poster from 'Components/Poster';
+
+const Container = styled.div`
+	padding: 0 20px;
+`;
+
+const Form = styled.form`
+	margin-bottom: 50px;
+`;
+
+const Input = styled.input`
+	all: unset;
+	font-size: 28px;
+	width: 100%;
+`;
+
+const SearchPresenter = ({ movieResults, tvResults, searchTerm, loading, error, handleSubmit, updateTerm }) => (
+	<Container>
+		<Helmet>
+			<title>Search | Alleyflix</title>
+		</Helmet>
+		<Form onSubmit={handleSubmit}>
+			<Input placeholder="Search Movies or TV Shows..."
+			       value={searchTerm}
+			       onChange={updateTerm}/>
+		</Form>
+
+		{loading ? <Loader/> : (
+			<>
+				{movieResults && movieResults.length > 0 && (
+					<Section title="Movie Results">
+						{movieResults.map(movie => (
+							<Poster
+								key={movie.id}
+								id={movie.id}
+								title={movie.title}
+								mageUrl={movie.poster_path}
+								rating={movie.vote_average}
+								year={movie.release_date.substring(0, 4)}
+								isMovie={true}
+							/>
+						))}
+					</Section>
+				)}
+
+				{tvResults && tvResults.length > 0 && (
+					<Section title="TV Results">
+						{tvResults.map(show => (
+							<Poster
+								key={show.id}
+								id={show.id}
+								title={show.name}
+								imageUrl={show.poster_path}
+								rating={show.vote_average}
+								year={show.first_air_date.substring(0, 4)}
+							/>
+						))}
+					</Section>
+				)}
+			</>
+		)}
+
+		{error && <Message color="#e74c3c" text={error}/>}
+
+		{movieResults &&
+		tvResults &&
+		tvResults.length === 0 &&
+		movieResults.length === 0 && (
+			<Message text={`Nothing Found: ${searchTerm}`} color="#95afc0"></Message>
+		)}
+	</Container>
+);
+
+SearchPresenter.propTypes = {
+	movieResults: PropTypes.array,
+	tvResults: PropTypes.array,
+	searchTerm: PropTypes.string,
+	loading: PropTypes.bool.isRequired,
+	error: PropTypes.string,
+	handleSubmit: PropTypes.func.isRequired,
+	updateTerm: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
